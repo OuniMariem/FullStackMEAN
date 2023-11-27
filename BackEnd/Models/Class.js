1949a4d243bbca445e8c12b2239e1b51862f1a6e
OuniMariem <ounimariem4@gmail.com>
2023-11-27 13:00:41 +0100

    version final

---
+++ BackEnd/Models/Class.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ClassesSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    building: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+    },
+    capacityOfStudent: {
+      type: Number,
+      required: true,
+    },
+  },
+  { collection: "Class" }
+);
+
+module.exports = mongoose.model("Class", ClassesSchema);
